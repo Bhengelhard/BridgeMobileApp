e82266312b962289f32d7daa2677b8cf5bbfdf85
Blake Engelhard <bhengelhard@gmail.com>
2016-11-10 19:59:59 -0500

    created a nicer folder structure within Custom Classes and created the SwipeCard.swift file

---
+++ Custom Classes/Custom Views/SwipeCard.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeCard.swift
+//  MyBridgeApp
+//
+//  Created by Blake Engelhard on 11/10/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+/*class SwipeCard: UIView {
+    
+    override init (frame: GCRect) {
+        super.init(frame:frame)
+        
+        
+    }
+    
+    
+}*/
