5b788c2ac1fd4bc89723fa565cfa737eda44609e
Abraham Vegh <me@abrahamvegh.com>
2017-01-05 14:20:09 -0500

    Call updateApplicationBadge from applicationDidBecomeActive

--- MyBridgeApp/AppDelegate.swift
+++ MyBridgeApp/AppDelegate.swift
@@ -202,6 +202,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         FBSDKAppEvents.activateApp()
+		PFCloudFunctions().updateApplicationBadge()
     }
     
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
