527009841e8742c22775ea308e9b90e7ec37915c
Blake Engelhard <bhengelhard@gmail.com>
2017-05-17 16:00:56 -0400

    scripts adding to randomize the BridgePairings with scores of 0 to between 1 and 499 and to randomize the BridgePairings of people in the same city with scores between 500 and 999

--- Scripts/Scripts.swift
+++ Scripts/Scripts.swift
@@ -52,4 +52,76 @@
 //            }
 //        }
 //    }
+//    
+//    static func randomizeBridgeScores() {
+//        print("randomize bridge scores called")
+//        let query = PFQuery(className: "BridgePairings")
+//        query.whereKey("Bridged", notEqualTo: true)
+//        query.whereKey("score", equalTo: 0)
+//        
+//        query.limit = 10000
+//        query.findObjectsInBackground { (objects, error) in
+//            if error == nil {
+//                if let objects = objects {
+//                    var countOfSaved = 0
+//                    var i = 0
+//                    print("objects count \(objects.count)")
+//                    for object in objects {
+//                        object["score"] = i
+//                        object.saveInBackground(block: { (success, error) in
+//                            countOfSaved += 1
+//                            
+//                            if success {
+//                                print("Count of Saved - \(countOfSaved)")
+//                            }
+//                        })
+//                        
+//                        print("i is \(i)")
+//                        i += 1
+//                        i = i%500
+//                    }
+//                }
+//            }
+//        }
+//        
+//    }
+//    
+//    
+//    static func increaseSameCityBridgeScores() {
+//        print("increaseSameCityBridgeScores called")
+//        let query = PFQuery(className: "BridgePairings")
+//        query.whereKey("Bridged", notEqualTo: true)
+//        query.whereKeyExists("user1_city")
+//        query.whereKeyExists("user2_city")
+//        
+//        query.limit = 10000
+//        query.findObjectsInBackground { (objects, error) in
+//            if error == nil {
+//                if let objects = objects {
+//                    var countOfSaved = 0
+//                    var i = 1
+//                    print("objects count \(objects.count)")
+//                    for object in objects {
+//                        if let user2City = object["user2_city"] as? String {
+//                            if let user1City = object["user1_city"] as? String {
+//                                if user2City == user1City {
+//                                    print("i is \(i)")
+//                                    object["score"] = i+499
+//                                    object.saveInBackground(block: { (success, error) in
+//                                        if success {
+//                                            countOfSaved += 1
+//                                            print("Count of Saved - \(countOfSaved)")
+//                                        }
+//                                    })
+//                                }
+//                            }
+//                        }
+//                        i += 1
+//                        i = i%500
+//                    }
+//                }
+//            }
+//        }
+//        
+//    }
 //}
